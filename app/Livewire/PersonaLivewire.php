8340c401f4e97ee0efb1d7a3a1a43e6aa66bca95
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2024-03-19 19:30:12 -0400

    FUNCIONALIDADES PARA EL CASO DE USO PERSONA MOSTRAR

--- app/Livewire/PersonaLivewire.php
+++ app/Livewire/PersonaLivewire.php
@@ -4,18 +4,32 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Persona;
+use Livewire\WithPagination;
 
 class PersonaLivewire extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+    public $search;
+    public $searchPersona;
+    public $obtenerIdPersona = "";
+
     public function render()
     {
-        $personas = Persona::where('nombre_pers', 'like', '%' . $this->buscador . '%')
-                                    ->orWhere('apellido_pers', 'like', '%' . $this->buscador . '%')
-                                    ->orWhere('ci_pers', 'like', '%' . $this->buscador . '%')
-                                    ->orWhere('correo_pers', 'like', '%' . $this->buscador . '%')
-                                    ->orderBy('id_persona','desc')
+        $personas = Persona::where('nombre_pers', 'like', '%' . $this->search . '%')
+                                    ->orWhere('apellido_pers', 'like', '%' . $this->search . '%')
+                                    ->orWhere('ci_pers', 'like', '%' . $this->search . '%')
+                                    ->orWhere('correo_pers', 'like', '%' . $this->search . '%')
+                                    ->orderBy('id','desc')
                                     ->paginate(5);
 
         return view('livewire.persona.persona-livewire', compact('personas'));
     }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
 }
--- resources/views/livewire/persona/persona-livewire.blade.php
+++ resources/views/livewire/persona/persona-livewire.blade.php
@@ -1,64 +1,67 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
-</div>
-<div>
-    <div class="card">
-        <div class="card-header">
-            <div class="row d-flex justify-content-between">
-                <div class="col-md-1">
-                    <button class="btn btn-success btn-md" wire:click="$toggle('openModalNew')"><i class="fas fa-plus-square"></i></button>
-                </div>
-                <div class="col-md-4">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+{{-- **** CONTENEDOR **** --}}
+        <div class="card">
+            <div class="card-header">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-md-1">
+                        <button class="btn btn-success btn-md" wire:click="$toggle('openModalNew')"><i class="fas fa-plus-square"></i></button>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+                            </div>
+                            {{-- <input type="text" class="form-control" placeholder="buscar" aria-label="buscar" aria-describedby="basic-addon1" wire:model.live="search"> --}}
                         </div>
-                        {{-- <input type="text" class="form-control" placeholder="buscar" aria-label="buscar" aria-describedby="basic-addon1" wire:model.live="search"> --}}
-                      </div>
+                    </div>
                 </div>
-            </div>
 
-        </div>
-        @if($users->count())
-        <div class="card-body">
-            <table class="table table-bordered table-hover table-light">
-                <thead class="thead-light">
-                    <tr class="text-center">
-                        <th>#</th>
-                        <th>NOMBRE</th>
-                        <th>EMAIL</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($personas as $persona)
-                        <tr>
-                            <td class="text-center">{{ $loop->iteration }}</td>
-                            <td>{{$persona->nombre_pers}}</td>
-                            <td>{{$persona->apellido_pers}}</td>
-                            <td class="text-center">
-                                {{-- @can('register') --}}
-                                <button class="btn btn-warning btn-sm" wire:click="edit({{ $user->id }})"><i class="fas fa-pencil-alt"></i></button>
-                                {{-- @endcan --}}
-                                @if ($user->estado == 1)
-                                      <button class="btn btn-danger btn-sm" wire:click="$dispatch('confirmDelete', {{ $persona->id }})"><i class="fas fa-trash"></i></button>
-                                @else
-                                      <button class="btn btn-primary btn-sm" wire:click="$dispatch('confirmDelete', {{ $persona->id }})"><i class="fas fa-history"></i></button>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="card-footer">
-            {{$personas->links()}}
-        </div>
-        @else
+            </div>
+            @if($personas->count())
             <div class="card-body">
-                <strong class="text-center">No hay registros</strong>
+                <table class="table table-sm table-bordered table-hover ">
+                    <thead class="thead-light table-dark">
+                        <tr class="text-center">
+                            <th>#</th>
+                            <th>NOMBRE</th>
+                            <th>APELLIDO</th>
+                            <th>CI</th>
+                            <th>CORREO</th>
+                            <th>TELEFONO</th>
+                            <th>ACCIONES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($personas as $persona)
+                            <tr>
+                                <td class="text-center">{{ $loop->iteration }}</td>
+                                <td>{{$persona->nombre_pers}}</td>
+                                <td>{{$persona->apellido_pers}}</td>
+                                <td class="text-center">
+                                    {{-- @can('register') --}}
+                                    {{-- <button class="btn btn-warning btn-sm" wire:click="edit({{ $user->id }})"><i class="fas fa-pencil-alt"></i></button> --}}
+                                    {{-- @endcan --}}
+                                    {{-- @if ($user->estado == 1)
+                                        <button class="btn btn-danger btn-sm" wire:click="$dispatch('confirmDelete', {{ $persona->id }})"><i class="fas fa-trash"></i></button>
+                                    @else
+                                        <button class="btn btn-primary btn-sm" wire:click="$dispatch('confirmDelete', {{ $persona->id }})"><i class="fas fa-history"></i></button>
+                                    @endif --}}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-        @endif
-    </div>
+            <div class="card-footer">
+                {{$personas->links()}}
+            </div>
+            @else
+                <div class="card-body">
+                    <strong class="text-center">No hay registros</strong>
+                </div>
+            @endif
+        </div>
 
+{{-- **** FIN CONTENEDOR **** --}}
 </div>
+
